11b619af2b466dd8b17c17177fb513565c5fe8c5
adilaroisa <adilaroisa688@gmail.com>
2024-03-26 10:25:47 +0700

    Membuat parameter

--- PemdasSwitchCasePertemuan2603204/PemdasSwitchCasePertemuan2603204.cpp
+++ PemdasSwitchCasePertemuan2603204/PemdasSwitchCasePertemuan2603204.cpp
@@ -14,6 +14,17 @@ int fungsiluas(int p, int l)
 
 int main()
 {
-   
+	int panjang, lebar;
+	cout << "Masukan Panjang: ";
+	cin >> panjang;
+	cout << "Masukan lebar: ";
+	cin >> lebar;
+
+	prosedurluas(panjang, lebar);
+	cout << "Luas persegi panjang dengan prosedur: " << luas << endl;
+
+	cout << "Luas persegi panjang dengan fungsi: " << fungsiluas(panjang, lebar) << endl;
+
+	return 0;
 }
 
